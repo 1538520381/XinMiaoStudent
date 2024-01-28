05b7ee4301c3f293216f186e3a90fda54311b3ff
Persolute <1538520381@qq.com>
2024-01-28 19:29:08 +0800

    MyMetaObjectHandler.java

---
+++ src/main/java/study/common/MyMetaObjectHandler.java
@@ -0,0 +1,41 @@
+package study.common;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Persolute
+ * @version 1.0
+ * @description 公共字段自动填充
+ * @email 1538520381@qq.com
+ * @date 2024/1/28 19:19
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    // 新增时自动填充
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        metaObject.setValue("createTime", LocalDateTime.now());
+        metaObject.setValue("updateTime", LocalDateTime.now());
+        if (metaObject.hasGetter("createUser")) {
+            metaObject.setValue("createUser", BaseContext.getCurrentId());
+        }
+        if (metaObject.hasGetter("updateUser")) {
+            metaObject.setValue("updateUser", BaseContext.getCurrentId());
+        }
+    }
+
+    // 修改时自动填充
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        metaObject.setValue("updateTime", LocalDateTime.now());
+        if (metaObject.hasGetter("updateUser")) {
+            metaObject.setValue("updateUser", BaseContext.getCurrentId());
+        }
+    }
+}
