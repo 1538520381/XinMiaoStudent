86b15af9eeb4e573f73fd703a005afa54f1cf946
Persolute <1538520381@qq.com>
2024-02-13 18:25:04 +0800

    SchoolController.java login

--- src/main/java/study/enums/HttpCodeEnum.java
+++ src/main/java/study/enums/HttpCodeEnum.java
@@ -20,13 +20,8 @@ public enum HttpCodeEnum {
     ACCOUNT_UNEXIST(504, "账号不存在"),
     PASSWORD_ERROR(505, "密码错误"),
     NO_PERMISSION(506, "无权限"),
-    SCHOOL_MISSING(507, "请选择学校"),
-    EMAIL_MISSING(508, "请输入邮箱"),
-    CODE_MISSING(509, "请输入验证码"),
-    CODE_WRONG(510, "验证码错误"),
-    REGISTER_FAIL(511, "注册失败"),
-    SEND_CODE_FAIL(512, "发送验证码失败"),
-    ILLEGAL_MAIL(513, "邮箱不合法");
+    SEND_CODE_FAIL(507, "发送验证码失败"),
+    ILLEGAL_MAIL(508, "邮箱不合法");
 
 
     private final int code;
